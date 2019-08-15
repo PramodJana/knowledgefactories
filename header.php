2b899b972eae3d152a3ee30e3fcccc954af483d6
PramodJana <Pramodjana395@gmail.com>
2019-08-15 20:00:03 +0530

    commit

---
+++ header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <link href="style.css" rel="stylesheet">
+    <!-- Required meta tags -->
+
+    <style>
+    .navbar-brand{
+    animation-iteration-count:infinite;}
+    </style>
+
+  </head>
+  <body>
+
+<?php
+echo '
+
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color:#0f9d58; height:75px">
+    <a class="navbar-brand wow pulse" href="index.php">Fine Knowledge</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home </a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">About Us </a>
+        </li><li class="nav-item active">
+          <a class="nav-link" href="#">Contact Us </a>
+        </li>
+      </ul>
+    </div>
+  </nav>';
+  ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    </body>
+</html>
