1dd7e94f842483031b1833874dce424436f9359f
PramodJana <Pramodjana395@gmail.com>
2019-10-13 17:55:05 +0530

    Working on quiz management system 1

---
+++ QUIZ_MANG/registration.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+header('location:login.php');
+
+$con = mysqli_connect('localhost','root');
+if($con)
+{
+    echo "Connection sucessful";
+}
+else{
+    echo "no connection";
+}
+
+mysqli_select_db($con, 'sessionpractical');
+
+$name = $_POST['user'];
+$pass= $_POST['password'];
+
+
+$q="select * from signin where name='$name' && password='$pass' ";
+
+$result= mysqli_query($con,$q);
+
+$num= mysqli_num_rows($result);
+
+if($num>0)
+{
+    echo "duplicate data";
+}
+else
+{
+    $qy="insert into signin(name,password) values('$name', '$pass')";
+    mysqli_query($con,$qy);
+}
+
+
+
+
+?>
